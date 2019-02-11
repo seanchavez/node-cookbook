a53da02d486eeefe947f0efbbaa121c25f8adffa
Sean Chavez <realseanchavez@gmail.com>
2019-02-10 21:13:33 -0800

    scrape API urls from doc page

--- cheerio-example/index.js
+++ cheerio-example/index.js
@@ -2,7 +2,19 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 
 axios.get('https://buttercms.com/docs/api/').then(response => {
-  console.log(response.data);
+  //console.log(response.data);
+  const $ = cheerio.load(response.data);
+
+  const urlElems = $('pre.highlight.shell');
+
+  for (let i = 0; i < urlElems.length; i++) {
+    const urlSpan = $(urlElems[i]).find('span.s1')[0];
+
+    if (urlSpan) {
+      const urlText = $(urlSpan).text();
+      console.log(urlText);
+    }
+  }
 });
 
 // const $ = cheerio.load(
